704539d7823d95176c7dc945cd975e5fd383516e
Alekto <alektotaras@yandex.ru>
2018-05-18 22:21:48 +0300

    dev: Few changes

--- view.py
+++ view.py
@@ -1,14 +1,14 @@
 import re, datetime
 from flask_restful import Resource
 from db import db
-#from flask import request
+from flask import request
 
 class GetFormResource(Resource):
 
     EMAIL_RE = re.compile(r'(^|\s)[-a-z0-9_.]+@([-a-z0-9]+\.)+[a-z]{2,6}(\s|$)')
-    PHONE_RE = re.compile(r'\+7 [0-9][0-9][0-9]'
-                         r' [0-9][0-9][0-9]'
-                         r' [0-9][0-9] [0-9][0-9]')
+    PHONE_RE = re.compile(r"\s7\s[0-9][0-9][0-9]"
+                          r"\s[0-9][0-9][0-9]"
+                          r"\s[0-9][0-9]\s[0-9][0-9]")
     @classmethod
     def gettype(cls,s):
         type = ''
@@ -22,13 +22,16 @@ class GetFormResource(Resource):
                 type = 'date'
             except ValueError:
                 type = 'text'
+
         return type
 
 
     def get(self):
+        #+7%20916%20243%2032 % 2003
         return "<http>You need to use POST method</http>"
 
 
     def post(self):
-        s = '@asdasdasd.com'
-        return 'DB: {} && type:{}'.format(db.all(),GetFormResource.gettype(s))
+        #print(request.args.get('c'))7... А НЕ +7...
+        #[v for k, v in request.args.items()]
+        return {k:GetFormResource.gettype(v) for k,v in request.args.items()}
