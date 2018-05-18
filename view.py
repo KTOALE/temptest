e4de5187d580ca19f00ac1c628b230dcbd70f06d
Alekto <alektotaras@yandex.ru>
2018-05-18 18:20:53 +0300

    Validation

--- view.py
+++ view.py
@@ -1,16 +1,34 @@
+import re, datetime
 from flask_restful import Resource
-from db import db
-from flask import request
+#from db import db
+#from flask import request
 
 class GetFormResource(Resource):
 
+    EMAIL_RE = re.compile(r'(^|\s)[-a-z0-9_.]+@([-a-z0-9]+\.)+[a-z]{2,6}(\s|$)')
+    PHONE_RE = re.compile(r'\+7 [0-9][0-9][0-9]'
+                         r' [0-9][0-9][0-9]'
+                         r' [0-9][0-9] [0-9][0-9]')
+    @classmethod
+    def gettype(cls,s):
+        type = ''
+        if cls.EMAIL_RE.match(s):
+            type = 'email'
+        elif cls.PHONE_RE.match(s):
+            type = 'phone'
+        else:
+            try:
+                datetime.datetime.strptime(s, '%Y-%m-%d')
+                type = 'date'
+            except ValueError:
+                type = 'text'
+        return type
+
+
     def get(self):
-        return "<http>WTF??? *UCK THS SHIT</http>"
+        return "<http>You need to use POST method</http>"
 
 
     def post(self):
-        s = ''
-        for k, v in request.args.items():
-            s += k + ' : ' + v + '\n'
-
-        return s
+        s = '@asdasdasd.com'
+        return self.gettype(s)
