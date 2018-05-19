cb8f68b6420fd29da984bd5c601afd34753a1a65
Alekto <alektotaras@yandex.ru>
2018-05-19 21:22:23 +0300

    dev: few fixes

--- test/UT.py
+++ test/UT.py
@@ -1,10 +1,11 @@
 import unittest
 import requests
+from test import *
 
 class TestsWithRequests(unittest.TestCase):
 
     def is_temp1(self):
-        template = requests.post('http://localhost:7777/getform?f1=asdasdasd@qweqwe.we&f2=2017-07-19')
+        template = requests.post('http://localhost:7777/get_form?f1=asdasdasd@qweqwe.we&f2=2017-07-19')
         self.assertEqual(template,'temp1')
 
 if __name__=="__main__":
